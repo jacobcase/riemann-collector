0950ed84a8751fc891abaf173286a772c83434c4
Jacob <jacobcase94@ksu.edu>
2015-03-18 21:41:48 -0500

    basic event shipping works, much left to do

--- client/client.go
+++ client/client.go
@@ -17,7 +17,7 @@ func RunClients(servers []config.ServerConfig, eventChan chan *rproto.Event) {
     for _, conf := range servers {
         tmpChan := make(chan *rproto.Event, 10)
         clientChans = append(clientChans, tmpChan)
-        go EventClientTCP(conf, tmpChan)        
+        go MsgClientTCP(conf, tmpChan)        
     }
 
 
@@ -44,74 +44,36 @@ func RunClients(servers []config.ServerConfig, eventChan chan *rproto.Event) {
 }
 
 
-func EventClientTCP(conf config.ServerConfig, events chan *rproto.Event) {
+func MsgClientTCP(conf config.ServerConfig, events chan *rproto.Event) {
     addr, _ := net.ResolveTCPAddr("tcp", conf.Host)
     conn, _ := net.DialTCP("tcp", nil, addr)
 
-
-//    eventBuf := proto.NewBuffer(nil)
-
     sizeBytes := make([]byte, 4, 4)
 
-//    msgBuf := proto.NewBuffer(nil)
-//    msgBytes := make([]byte, 256, 256)
-//    msgMsg := &rproto.Msg{}
+    msg := &rproto.Msg{
+            Events: make([]*rproto.Event, 1, 1),
 
-//    msgEvent := &rproto.Event{}
+        }
 
     for ev := range events {
         log.Println("Event recieved: ", ev)
 
-//        err = eventBuf.Marshal(ev)
-        data, err := proto.Marshal(ev)
+        msg.Events[0] = ev
+
+        data, err := proto.Marshal(msg)
         if err != nil {
             panic(err)
         }
 
-//        binary.BigEndian.PutUint32(sizeBytes, uint32(len(eventBuf.Bytes())))
         binary.BigEndian.PutUint32(sizeBytes, uint32(len(data)))
-        log.Println("Buffer size measured: ", len(data))
-
-//        _, err = conn.Write(sizeBytes)
-        log.Println("Buffer size: ", sizeBytes)
-
-        if err != nil {
-            panic(err)
-        }
 
         sizeBytes = append(sizeBytes, data...)
         _, err = conn.Write(sizeBytes)
-
         if err != nil {
             panic(err)
         }
 
         sizeBytes = sizeBytes[:4]
-
-//        proto.Unmarshal(data, msgEvent)
-
-//        log.Println("Event converted: ", msgEvent)
-
-//        eventBuf.Reset()
-//        sizeBytes := msgBytes[:4]
-//        conn.Read(sizeBytes)
-//        msgBuf.SetBuf(sizeBytes)
-//        msgSize, _ := msgBuf.DecodeFixed32()
-//        log.Println("Msg size recieved, size: ", msgSize)
-//
-//
-//        if msgSize > uint64(len(msgBytes)) {
-//            msgBytes = make([]byte, msgSize, msgSize)
-//        }
-//
-//        conn.Read(msgBytes[:msgSize])
-//        msgBuf.SetBuf(msgBytes[:msgSize])
-//
-//        msgBuf.Unmarshal(msgMsg)
-//
-//        log.Println(msgMsg)
-
-        
 
     }    
 }
--- scripts/load.sh
+++ scripts/load.sh
@@ -7,13 +7,19 @@ LOAD=`cat /proc/loadavg | awk '{print $1}'`
 cat << EOF
 {
     "time": $DATE,
+    "description": "woop woop woop",
     "state": "ok",
     "service": "system",
     "host": "$HOST",
-    "metric_d": $LOAD
+    "metric_d": $LOAD,
     "tags": [
         "woop"
-        ]
-    "ttl": 10.1
+        ],
+    "ttl": 10
+
 }
 EOF
+
+#cat << EOF
+#{"host": "wut","service": "testing","metric_d": 2.5}
+#EOF
